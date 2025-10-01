6d779f92bb0dae197556fd5c0e6145f32d73c739
Mikhail Masagutov <mmasagutov@company.local>
2025-10-01 15:26:17 +0300

    Add draft of k-means

---
+++ unsupervised/k_means_clustering/k_means.py
@@ -0,0 +1,22 @@
+class MyKMeans():
+    def __init__(
+        self,
+        n_clusters=3,
+        max_iter=10,
+        n_init=3,
+        random_state=42
+    ):
+        self.n_clusters = n_clusters
+        self.max_iter = max_iter
+        self.n_init = n_init
+        self.random_state = random_state
+
+    def __str__(self):
+        params = [
+            "n_clusters",
+            "max_iter",
+            "n_init",
+            "random_state"
+        ]
+        attrs = ", ".join(f"{p}={getattr(self, p)}" for p in params)
+        return f"{self.__class__.__name__} class: {attrs}"
